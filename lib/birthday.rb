981d0956b2f82673659743b31bea5b85012e6f2e
Tanya Bihag <tbihag@gmail.com>
2019-08-25 19:29:28 +0000

    Automatically backed up by Learn

--- lib/birthday.rb
+++ lib/birthday.rb
@@ -10,9 +10,3 @@ def happy_birthday(birthday_kids)
   end
 end
 
-def age_appropriate_birthday(birthday_kids)
-  birthday_kids.collect do |kids_name, age|
-    while age == age < 12
-      puts "Happy birthday #{kids_name}! You are now #{age} years old!"
-    end
-end
