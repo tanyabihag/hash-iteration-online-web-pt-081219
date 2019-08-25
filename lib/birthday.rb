f3a9676afe9bb1c35b94d14f9e3162ff0a5b33c0
Tanya Bihag <tbihag@gmail.com>
2019-08-25 19:19:26 +0000

    Automatically backed up by Learn

--- lib/birthday.rb
+++ lib/birthday.rb
@@ -11,3 +11,15 @@ def happy_birthday(birthday_kids)
 end
 
 
+
+
+def select_winner(passengers)
+  winner = ""
+passengers.each do |suite, name|
+  if suite == :suite_a && name.start_with?("A")
+    winner = name
+  end
+end
+ 
+winner
+end
