82650001624721b3535b94b252a45609ca02a32e
Tanya Bihag <tbihag@gmail.com>
2019-08-25 19:23:22 +0000

    Automatically backed up by Learn

--- lib/birthday.rb
+++ lib/birthday.rb
@@ -12,17 +12,7 @@ end
 
 def age_appropriate_birthday(birthday_kids)
   birthday_kids.each do |kids_name, age|
-    
-
-
-
-def select_winner(passengers)
-  winner = ""
-passengers.each do |suite, name|
-  if suite == :suite_a && name.start_with?("A")
-    winner = name
-  end
-end
- 
-winner
+    if kids_name == age < 12
+      puts "Happy birthday #{kids_name}! You are now #{age} years old!"
+    end
 end
